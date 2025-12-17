de700bc8eb48aadac29b063f1536ffade224ecbb
dicoaensenada <ventasdicoaensenada@gmail.com>
2025-12-17 12:51:31 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'dicoa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/clientes.html',
+  '/bitacora.html',
+  '/Inventario.html',
+  '/gastos.html',
+  '/pedidos.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
